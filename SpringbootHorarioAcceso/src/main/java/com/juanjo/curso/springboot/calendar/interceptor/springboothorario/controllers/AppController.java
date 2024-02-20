08a2f7083ed8c42e0227f2562682814ed208d41d
juanjojur97 <120376406+juanjojur97@users.noreply.github.com>
2024-02-20 20:13:20 +0100

    add project

---
+++ SpringbootHorarioAcceso/src/main/java/com/juanjo/curso/springboot/calendar/interceptor/springboothorario/controllers/AppController.java
@@ -0,0 +1,32 @@
+package com.juanjo.curso.springboot.calendar.interceptor.springboothorario.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AppController {
+    @GetMapping("/foo")
+    public ResponseEntity<?> foo(HttpServletRequest request) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("title","Bienvenidos al sistema de atención.");
+        data.put("time",new Date());
+        data.put("message", request.getAttribute("message"));
+        return ResponseEntity.ok(data);
+    }
+}
+
+
+
+
+
+
+
+
+
+
